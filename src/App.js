6b838a74ad29d8890ce0c0dba4caa9a7ff61cec6
MarcusMadMax <marcuszillner@icloud.com>
2020-08-07 11:07:22 +1200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,94 +1,10 @@
 import React, {Component} from 'react';
-import NewTodoForm from './NewTodoForm';
-import Todo from './Todo';
-import './App.css';
+import Joke from './Joke'
 
-class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      todos:[
-
-      ]
-    }
-  }
-
-  addTodo = (data)=>{
-
-    var newTodo = {
-      id: Date.now(),
-      ...data
-    }
-
-    var newList = [newTodo, ...this.state.todos]
-    this.setState({
-      todos: newList
-    })
-
-  }
-  removeTodo = (id)=>{
-    var todos = this.state.todos
-
-    var filtered = todos.filter((todo)=>{
-      return todo.id != id
-    })
-
-    this.setState({
-      todos: filtered
-    })
-
-  }
-
-  updateTodo = (id,data)=>{
-    // id = 1
-    // data = {
-    //   content: 'Water house plants and garden'
-    // }
-    var todos = this.state.todos
-    var updated = todos.map((todo)=>{
-      return (todo.id == id) ? {...todo,...data} : todo
-    })
-    this.setState({
-      todos:updated
-    })
-  } 
-
-  
-
-  render(){
-    return (
-      <div className="wrap">
-        <div className="container">
-          <div className="todos">
-
-         
-            {
-              this.state.todos.map((todo)=>{
-
-                var todoProps = {
-                  key: todo.id,
-                  removeTodo: this.removeTodo,
-                  updateTodo: this.updateTodo,
-                  ...todo
-                }
-                return (
-
-                  <Todo {...todoProps}/>
-
-                )
-              })
-
-            }
-            
-       
-            <NewTodoForm addTodo={this.addTodo}/>
-
-          </div>
-        </div>
-      </div>
-    )
-  }
-
+function App(){
+  return(
+    <Joke />
+  )
 }
 
-export default App;
+export default App
---
+++ src/Joke.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function Joke(){
+    return(
+        <h1>Hello</h1>
+    )
+}
+
+export default Joke
